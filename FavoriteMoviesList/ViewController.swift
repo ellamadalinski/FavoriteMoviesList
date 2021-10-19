4a12980d284048bfbf419feeed7c0d9b7428005f
Ella Madalinski <ella.madalinski24@d155.org>
2021-10-19 07:29:10 -0500

    Initial Commit

---
+++ FavoriteMoviesList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FavoriteMoviesList
+//
+//  Created by ELLA MADALINSKI on 10/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
