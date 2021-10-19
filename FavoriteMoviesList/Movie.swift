0121790a721602389e6710b096f7c55221f9cd7a
Ella Madalinski <ella.madalinski24@d155.org>
2021-10-19 08:07:59 -0500

    initial commit

---
+++ FavoriteMoviesList/Movie.swift
@@ -0,0 +1,27 @@
+//
+//  Movie.swift
+//  FavoriteMoviesList
+//
+//  Created by ELLA MADALINSKI on 10/19/21.
+//
+
+import Foundation
+
+public class Movie{
+    
+    var name : String
+    var yearReleased : Int
+    var rating : Double
+    var image : String
+    var discription : String
+
+
+    init(n : String, y : Int, r : Double, i : String, d : String){
+        name = n
+        yearReleased = y
+        rating = r
+        image = i
+        discription = d
+    }
+    
+}
--- FavoriteMoviesList/ViewController.swift
+++ FavoriteMoviesList/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource{
 
+    var favs : [String] = ["Interstellar", "Titanic", "Gifted", "Harry Potter", "Teen Beach Movie", "High School Musical", "How the Grinch Stole Christmas", "8", "9", "10", "11", "12"]
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
     }
-
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableViewOutlet.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = "\(favs[indexPath.row])"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
 }
 
